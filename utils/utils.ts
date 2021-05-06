7082ddc0a36b9437516f8c88f4fa795bd2096541
suin-d <everytheone@gmail.com>
2021-05-06 18:56:15 +0900

    Add @dayjs and formatDate/CountSum utils

---
+++ utils/utils.ts
@@ -0,0 +1,13 @@
+import dayjs from 'dayjs';
+
+export const formatDate = (date: string): string => {
+  const dayjsDate = dayjs(date);
+  const formatDateResult = dayjsDate.format('MMM D, YYYY');
+
+  return formatDateResult;
+};
+
+export const CountSum = (arrayData: number[]): number => {
+  const countSumData = arrayData.reduce((acc, cur) => acc + cur, 0);
+  return countSumData;
+};
