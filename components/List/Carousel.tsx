e4137684367d871f894d35f1c3752d3bc869badc
conrad <yhg0337@naver.com>
2021-06-02 01:54:49 +0900

    Add useCarousel hooks

---
+++ components/List/Carousel.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import useCarousel from '../../hooks/useCarousel';
+
+const Container = styled.div<{ width: number }>`
+  width: 100%;
+  height: 526px;
+  position: relative;
+  overflow: hidden;
+  section {
+    width: ${({ width }) => width}px;
+    margin: 0 auto;
+  }
+`;
+const Slides = styled.div<{
+  duration: number;
+  currentSlide: number;
+  width: number;
+  scale: number;
+}>`
+  display: inline-flex;
+  transition: all ${({ duration }) => duration}ms ease-in-out;
+  transform: translateX(${({ currentSlide, width }) => currentSlide * -width}px);
+  & > div {
+    padding: 0 27px;
+    transition: 0.2s;
+  }
+  & > div:nth-child(${({ currentSlide }) => currentSlide + 1}) {
+    transform: scale(${({ scale }) => scale});
+    text-shadow: 0 0 3px ${({ theme }) => theme.grayScale[2]};
+  }
+`;
+
+const Control = styled.button`
+  position: absolute;
+  top: 50%;
+  width: 38px;
+  height: 38px;
+  display: flex;
+  font-size: 22px;
+  border-radius: 50%;
+  outline: none;
+  border: none;
+  align-items: center;
+  justify-content: center;
+  transform: translateY(-50%);
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.background.initial};
+  color: ${({ theme }) => theme.textColor.initial};
+  border: 1px solid ${({ theme }) => theme.primary[1]};
+`;
+const PrevControl = styled(Control)`
+  left: 0;
+`;
+const NextControl = styled(Control)`
+  right: 0;
+`;
+
+type CarouselPropsType = {
+  children: React.ReactNode;
+  width?: number;
+  listLength?: number;
+};
+export default function Carousel({
+  children,
+  width = 0,
+  listLength = 1,
+}: CarouselPropsType): JSX.Element {
+  const {
+    currentSlide,
+    duration,
+    isMoving,
+    move,
+    setIsMoving,
+    scale,
+    setScale,
+  } = useCarousel(listLength);
+
+  const handleClick: React.MouseEventHandler<HTMLElement> = e => {
+    if (isMoving) return;
+    const delta = e.target.id === 'prev' ? -1 : 1;
+    move(currentSlide + 1 * delta, 300);
+  };
+
+  const onTransitionEnd = () => {
+    setIsMoving(false);
+    const delta =
+      currentSlide === listLength - 1 ? 1 : currentSlide === listLength * 2 + 1 ? -1 : 0;
+    if (delta) move(currentSlide + listLength * delta);
+    setScale(1.13);
+  };
+
+  return (
+    <Container width={width}>
+      <section>
+        <Slides
+          scale={scale}
+          width={width}
+          currentSlide={currentSlide}
+          duration={duration}
+          onTransitionEnd={onTransitionEnd}
+        >
+          {children}
+        </Slides>
+      </section>
+      <PrevControl id="prev" onClick={handleClick}>
+        &laquo;
+      </PrevControl>
+      <NextControl id="next" onClick={handleClick}>
+        &raquo;
+      </NextControl>
+    </Container>
+  );
+}
---
+++ hooks/useCarousel.tsx
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+
+type useCarouselReturnType = {
+  currentSlide: number;
+  duration: number;
+  isMoving: boolean;
+  setIsMoving: Dispatch<SetStateAction<boolean>>;
+  move: (_currentSlide: number, _duration?: number) => void;
+  scale: number;
+  setScale: Dispatch<SetStateAction<number>>;
+};
+
+export default function useCarousel(startIndex: number): useCarouselReturnType {
+  const [scale, setScale] = useState(1.13);
+  const [currentSlide, setCurrentSlide] = useState(startIndex);
+  const [duration, setDuration] = useState(0);
+  const [isMoving, setIsMoving] = useState(false);
+
+  const move = (_currentSlide: number, _duration = 0) => {
+    if (_duration) setIsMoving(true);
+    setScale(1);
+    setCurrentSlide(_currentSlide);
+    setDuration(_duration);
+  };
+
+  return { currentSlide, duration, isMoving, setIsMoving, move, scale, setScale };
+}
